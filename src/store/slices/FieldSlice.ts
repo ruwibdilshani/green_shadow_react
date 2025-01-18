8b7fe75dfd95f37d50ca2b27bbc23bca3aa2f1db
ruwibdilshani0921@gmail.com <ruwibdilshani0921@gmail.com>
2025-01-18 20:28:46 +0530

    store

---
+++ src/store/slices/FieldSlice.ts
@@ -0,0 +1,26 @@
+import {Field} from "../../model/Field.ts";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+const initialState: Field[] = []
+
+const fieldSlice = createSlice({
+    name: "field",
+    initialState,
+    reducers: {
+        saveField: (state, action : PayloadAction<Field>) => {
+            state.push(action.payload)
+        },
+        updateField: (state, action : PayloadAction<Field>) => {
+            return state.map((field: Field) => field.fieldCode === action.payload.fieldCode
+                ? action.payload
+                : field
+            );
+        },
+        deleteField: (state, action : PayloadAction<string>) => {
+            return state.filter((field: Field) => field.fieldCode !== action.payload)
+        }
+    }
+})
+
+export const {saveField, updateField, deleteField} = fieldSlice.actions
+export default fieldSlice.reducer

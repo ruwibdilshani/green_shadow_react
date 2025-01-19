db808d715ee23994e57095a1b58a52655cf1633e
ruwibdilshani <ruwibdilshani0921@gmail.com>
2025-01-19 18:29:31 +0530

    component done error fix

--- src/components/wall/Home.tsx
+++ src/components/wall/Home.tsx
@@ -1,7 +1,23 @@
 import backImg from '../../assets/home.jpg'
 import '../../css/components/Home.css'
+import {useEffect, useState} from "react";
 
 const Home = () => {
+    const [currentDate, setCurrentDate] = useState("");
+    const [currentTime, setCurrentTime] = useState("");
+
+    useEffect(() => {
+        const updateDateTime = () => {
+            const now = new Date();
+            setCurrentDate(now.toLocaleDateString());
+            setCurrentTime(now.toLocaleTimeString());
+        };
+        const intervalId = setInterval(updateDateTime, 1000);
+        updateDateTime();
+
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <div className="p-0 bg-transparent w-100 " id="home-page">
             <div className="dashboard-header d-flex justify-content-between">
@@ -12,8 +28,16 @@ const Home = () => {
 
             </div>
 
-            <div id = 'backImg'>
+            <div id='backImg'>
                 <img src={backImg}/>
+            </div>
+
+            <div className="flex items-center justify-center mt-2 ">
+                <div className="  rounded-lg p-6 text-center">
+                    <h1 className="text-2xl font-bold 	">Current Date & Time</h1>
+                    <p className="mt-4 text-lg 	">Date: {currentDate}</p>
+                    <p className="text-xl font-semibold 	">Time: {currentTime}</p>
+                </div>
             </div>
 
         </div>
